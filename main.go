cba2eb1a36d716e3a6224edd6f70fb876013b283
cmbigk <bigk89865@gmail.com>
2024-11-09 03:43:27 +0200

    editing

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ func main() {
 		fmt.Println("Please provide a valid text argument.")
 		return
 	}
+
+	// Parse escape sequences (like "\n") in the provided text argument.
+	// This function replaces any occurrences of `\n` with actual newline characters.
 	text := ParseEscapeSequences(os.Args[1])
 
+	// Load the ASCII art template from a file called "standard.txt".
+	// This function reads the file, stores ASCII representations of characters in a map, and returns the map.
 	asciiMap, err := LoadTemplate("standard.txt")
 	if err != nil {
 		fmt.Println("Error loading template:", err)
 		return
 	}
-
 	PrintASCII(asciiMap, text)
 }
