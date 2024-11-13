ab95f7f943939f00dab418e86b918978b525ce35
cmbigk <bigk89865@gmail.com>
2024-11-13 15:39:26 +0200

    okk

--- printingascii.go
+++ printingascii.go
@@ -2,42 +2,39 @@ package main
 
 import "fmt"
 
-// PrintASCII takes an ASCII map and a text input, and prints the corresponding
-// ASCII art representation of each character in the text based on the template.
 func PrintASCII(asciiMap map[rune][]string, text string) {
 
-	var maxLines int //use maxLines to find the maximum number of lines any character's ASCII representation occupies
-	// to ensure that we print the ASCII art correctly, with all lines aligned.
+	var maxLines int
 	for _, lines := range asciiMap {
 		if len(lines) > maxLines {
-			maxLines = len(lines) // Track the character with the most lines in its ASCII art
+			maxLines = len(lines)
 		}
 	}
 
-	// Split the input text into separate lines based on the newline character
 	lines := SplitTextByLines(text)
 
-	for _, line := range lines {
+	for lineIndex, line := range lines {
 		for i := 0; i < maxLines; i++ {
 			for _, char := range line {
-				// Check if the character exists in the ASCII map
+
 				asciiLines, exists := asciiMap[char]
 				if !exists {
 					fmt.Printf("Error: Character '%c' not found in ASCII map\n", char)
 					return
 				}
 
-				// If the current line index is within the character's ASCII lines, print the corresponding line
-				// Otherwise, print spaces for padding to keep the formatting aligned
 				if i < len(asciiLines) {
 					fmt.Print(asciiLines[i])
 				} else {
-					// Print blank spaces if the character's ASCII representation is shorter than the longest one
 					fmt.Print("        ")
 				}
 			}
+			if !(lineIndex == len(lines)-1 && i == maxLines-1) {
+				fmt.Println()
+			}
+		}
+		if lineIndex < len(lines)-1 {
 			fmt.Println()
 		}
-		fmt.Println()
 	}
 }
