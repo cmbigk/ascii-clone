625fd0c4c3002824d19eab0be0abc5f6593aff7d
cmbigk <bigk89865@gmail.com>
2024-11-25 20:44:24 +0200

    ok

--- main.go
+++ main.go
@@ -14,7 +14,18 @@ func main() {
 	// Parse escape sequences (like "\n") in the provided text argument.
 	// This function replaces any occurrences of `\n` with actual newline characters.
 	text := ParseEscapeSequences(os.Args[1])
+	template := os.Args[2]
 
+	templates := map[string]string{
+		"standard":   "standard.txt",
+		"shadow":     "shadow.txt",
+		"thinkertoy": "thinkertoy.txt",
+	}
+	templatePath, exists := templates[template]
+	if !exists {
+		fmt.Println("Template not found.")
+		return
+	}
 	// Load the ASCII art template from a file called "standard.txt".
 	// This function reads the file, stores ASCII representations of characters in a map, and returns the map.
 	asciiMap, err := LoadTemplate(templatePath)
--- printingascii.go
+++ printingascii.go
@@ -12,33 +12,31 @@ func PrintASCII(asciiMap map[rune][]string, text string) {
 
 	lines := SplitTextByLines(text)
 
-	for lineIndex, line := range lines {
+	chars := 0
+	for _, line := range lines {
+		chars += len(line)
+	}
+	if chars == 0 {
+		lines = lines[:len(lines)-1]
+	}
+
+	for _, line := range lines {
+
 		if line == "" {
-			// Only print a single blank line for empty input lines
 			fmt.Println()
-			continue
-		}
-		for i := 0; i < maxLines; i++ {
-			for _, char := range line {
-				asciiLines, exists := asciiMap[char]
-				if !exists {
-					fmt.Printf("Error: Character '%c' not found in ASCII map\n", char)
-					return
-				}
+		} else {
 
-				// Print the ASCII line for the character if available, otherwise space
-				if i < len(asciiLines) {
+			for i := 0; i < 8; i++ {
+				for _, char := range line {
+					asciiLines, exists := asciiMap[char]
+					if !exists {
+						fmt.Printf("Error: Character '%c' not found in ASCII map\n", char)
+						return
+					}
 					fmt.Print(asciiLines[i])
-				} else {
-					fmt.Print("        ")
 				}
+				fmt.Println()
 			}
-			fmt.Println() // Newline after each row of ASCII characters
 		}
-		// Only add a blank line if the next line is not empty
-		if lineIndex < len(lines)-1 && lines[lineIndex+1] != "" {
-			fmt.Println()
-		}
-
 	}
 }
